55b6dacd77a8d7df18677bd22f32365121c5c07d
Gabriel Fidelis Pereira <jpinformaticafoz@gmail.com>
2022-12-07 17:32:37 -0300

    'change_token_time'

--- Definitivo/app/Http/Controllers/AuthController.php
+++ Definitivo/app/Http/Controllers/AuthController.php
@@ -116,7 +116,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->guard('api')->factory()->getTTL() * 60
+            'expires_in' => auth()->guard('api')->factory()->getTTL() * 120
         ]);
     }
 
